6488afc24771d4f17f3f21faacf849a96f9ec0b5
yaswanth narikamalli <yaswanthnarikamali@gmail.com>
2023-05-11 11:04:47 +0530

    properly working

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
-const charecters="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_-+=;:'`~\\.\   0123456789";
-let output=document.getElementById("output");
+// const charecters= 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!\"$%&/()=?@~`\\.\';:+=^*_-0123456789';
+// let output=document.getElementById("output");
 
+
+// function randomValue(value){
+//     return Math.floor(Math.random()*value);
+// }
+// function genPassword(){
+//     let length=document.getElementById('length').value;
+//     document.getElementById("length-val").textContent=length;
+//     let str='';
+
+//     for( let i=0; i<length; i++){
+//         let random=randomValue(charecters.length);
+//         str +=charecters.charAt(random);
+//     }
+//     output.value=str;
+// }
+// genPassword();
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!\"$%&/()=?@~`\\.\';:+=^*_-0123456789';
+let output = document.getElementById("output");
 
 function randomValue(value){
     return Math.floor(Math.random()*value);
 }
-function genPassword(){
-    let length=document.getElementById('length').value;
-    document.getElementById("length-val").textContent=length;
-    let str='';
 
-    for( let i=0; i<length; i++){
-        let random=randomValue(charecters.length);
-        str +=charecters.charAt(random);
+function genPassword(){
+    let length = document.getElementById('length').value;
+    document.getElementById("length-val").textContent = length;
+    let str = '';
+
+    for(let i=0; i<length ; i++){
+        let random = randomValue(characters.length);
+        str += characters.charAt(random);
     }
-    output.value=str;
+    output.value = str;
+
+}
+
+function copyClipboard(){
+    output.select();
+    document.execCommand('copy');
+    alert("Password Copied!");
 }
 genPassword();
